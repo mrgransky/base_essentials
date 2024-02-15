7db5b880e97b51a90ea73ec26cc9f0f14cb4c246
Farid Alijani <farid.alijani@gmail.com>
2024-02-15 15:23:11 +0200

    collect statistics add

--- narvi_intractive_gpu.sh
+++ narvi_intractive_gpu.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-srun -J gpu_interactive  --partition=test --gres=gpu:teslav100:1 --time=00-04:00:00 --mem=16G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J gpu_interactive  --partition=test --gres=gpu:teslav100:1 --time=00-04:00:00 --mem=128G --ntasks=1 --cpus-per-task=8 --pty /bin/bash -i
 #module load git
 #module load pytorch
