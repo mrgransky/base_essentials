75758363d6d48725e75b0438223e93f0294d58c4
Farid Alijani <farid.alijani@gmail.com>
2024-05-16 11:26:04 +0300

    puhti cpu large check

--- puhti_intractive_cpu.sh
+++ puhti_intractive_cpu.sh
@@ -5,7 +5,7 @@
 
 # Image_Retrieval_TUNI
 # srun -J intrtv_cpuTEST --account=project_2004072 --partition=test --time=00-00:15:00 --mem=180G --ntasks=1 --cpus-per-task=8 --pty /bin/bash -i
-srun -J intrtv_cpu_small --account=project_2004072 --partition=small --time=00-01:15:00 --mem=180G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+srun -J intrtv_cpu_small --account=project_2004072 --partition=large --time=00-05:15:00 --mem=180G --ntasks=1 --cpus-per-task=2 --pty /bin/bash -i
 # srun -J intrtv_cpu_hugemem --account=project_2004072 --partition=hugemem --time=00-10:00:00 --mem=473G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 # srun -J cpu_interactive --account=project_2004072 --partition=interactive --time=00-07:00:00 --mem=75G --ntasks=1 --cpus-per-task=8 --pty /bin/bash -i
 
