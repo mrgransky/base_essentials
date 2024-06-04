d5abb0bafd7dba28af61e0557d20a8f46907c923
Farid Alijani <farid.alijani@gmail.com>
2024-06-04 14:26:55 +0300

    gpu test interactive

--- puhti_intractive_gpu.sh
+++ puhti_intractive_gpu.sh
@@ -6,10 +6,10 @@
 # $ source puhti_intractive_gpu.sh
 
 # never change!!!!! JUST TO TEST
-# srun -J intrctv_gpuTEST --account=project_2004072 --partition=gputest --gres=gpu:v100:4 --time=0-00:15:00 --mem=373G --ntasks=1 --cpus-per-task=16 --pty /bin/bash -i
+srun -J intrctv_gpuTEST --account=project_2004072 --partition=gputest --gres=gpu:v100:4 --time=0-00:15:00 --mem=373G --ntasks=1 --cpus-per-task=16 --pty /bin/bash -i
 
 # changing is allowed:
-srun -J gpu_interactive --account=project_2004072 --partition=gpu --gres=gpu:v100:1 --time=0-03:15:00 --mem=88G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
+# srun -J gpu_interactive --account=project_2004072 --partition=gpu --gres=gpu:v100:1 --time=0-03:15:00 --mem=88G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
 
 # works after exit and source:
 module load git
