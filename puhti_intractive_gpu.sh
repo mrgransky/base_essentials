2a95e4774ba55edef18452706cf7659bf914cdfb
Farid Alijani <farid.alijani@gmail.com>
2025-02-19 16:06:11 +0200

    new files add

--- puhti_intractive_gpu.sh
+++ puhti_intractive_gpu.sh
@@ -6,7 +6,7 @@
 # $ source puhti_intractive_gpu.sh
 
 # never change!!!!! JUST TO TEST
-srun -J gpu_intrctive_test --account=project_2009043 --partition=gputest --gres=gpu:v100:1 --time=0-00:15:00 --mem=64G --ntasks=1 --cpus-per-task=40 --pty /bin/bash -i
+srun -J gpu_intrctive_test --account=project_2009043 --partition=gputest --gres=gpu:v100:1 --time=0-00:15:00 --mem=32G --ntasks=1 --cpus-per-task=4 --pty /bin/bash -i
 
 # changing is allowed:
 # srun -J gpu_interactive --account=project_2009043 --partition=gpu --gres=gpu:v100:1 --time=0-23:59:59 --mem=188G --ntasks=1 --cpus-per-task=1 --pty /bin/bash -i
