eedbfd9bdb8a7d31b71e2f20017c12bb51f0b621
Farid Alijani <farid.alijani@gmail.com>
2026-08-28 16:17:53 +0300

    csc certificate gen

---
+++ get_csc_certificate.sh
@@ -0,0 +1,10 @@
+# Clone or download the script
+git clone https://github.com/CSCfi/certificate-helper-tool.git
+cd certificate-helper-tool
+
+# Make it executable (optional)
+chmod +x csc_cert.py
+
+# Put it somewhere convenient, e.g. ~/bin
+mkdir -p ~/bin
+cp csc_cert.py ~/bin/csc_cert
